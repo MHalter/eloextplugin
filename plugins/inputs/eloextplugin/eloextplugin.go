080d200224390ba9005e0ab90365976cfaf8a3b4
Mark Halter <m.halter@elo.com>
2023-07-07 00:31:54 +0200

    first running code

--- plugins/inputs/eloextplugin/eloextplugin.go
+++ plugins/inputs/eloextplugin/eloextplugin.go
@@ -12,7 +12,7 @@ import (
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
-type RandomNumberGenerator struct {
+type EloPerfData struct {
 	ValueName       string          `toml:"value_name"`
 	Min             int64           `toml:"min"`
 	Max             int64           `toml:"max"`
@@ -24,9 +24,9 @@ type RandomNumberGenerator struct {
 }
 
 func init() {
-	inputs.Add("rand", func() telegraf.Input {
-		return &RandomNumberGenerator{
-			ValueName:       "value",
+	inputs.Add("eloextplugin", func() telegraf.Input {
+		return &EloPerfData{
+			ValueName:       "checkoutSord",
 			Min:             0,
 			Max:             100,
 			SampleFrequency: config.Duration(1 * time.Second),
@@ -34,28 +34,35 @@ func init() {
 	})
 }
 
-func (r *RandomNumberGenerator) Init() error {
+func (r *EloPerfData) Init() error {
 	return nil
 }
 
-func (r *RandomNumberGenerator) SampleConfig() string {
+func (r *EloPerfData) SampleConfig() string {
+	r.Log.Infof("SampleConfig called")
 	return `
   ## Generates random numbers
-	[inputs.rand]
+	[inputs.eloextplugin]
 	# the name of the measurement to write out to.
-	# value_name = "value"
+	# value_name = "checkoutSord"
 	# min = 0
 	# max = 100
-	# sample_frequency = "1000ms"
+	# sample_frequency = "1s"
 `
 }
 
-func (r *RandomNumberGenerator) Description() string {
+func (r *EloPerfData) Description() string {
 	return "Generates a random number"
 }
 
-func (r *RandomNumberGenerator) Gather(a telegraf.Accumulator) error {
-	r.sendMetric(a)
+func (r *EloPerfData) Gather(a telegraf.Accumulator) error {
+	ticker := time.NewTicker(time.Duration(r.SampleFrequency))
+	defer ticker.Stop()
+
+	for range ticker.C {
+		r.sendMetric(a)
+	}
+
 	return nil
 }
 
@@ -78,11 +85,11 @@ func (r *RandomNumberGenerator) Gather(a telegraf.Accumulator) error {
 // 	return nil
 // }
 
-func (r *RandomNumberGenerator) Stop() {
+func (r *EloPerfData) Stop() {
 	r.cancel()
 }
 
-func (r *RandomNumberGenerator) sendMetric(a telegraf.Accumulator) {
+func (r *EloPerfData) sendMetric(a telegraf.Accumulator) {
 	n := rand.Int63n(r.Max-r.Min) + r.Min
 	a.AddFields("random",
 		map[string]interface{}{
